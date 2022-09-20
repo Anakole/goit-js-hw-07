81d869c841860771083341a41ed5f35d9124dddb
Anakole <104356239+Anakole@users.noreply.github.com>
2022-09-21 00:22:07 +0300

    edits

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -14,10 +14,10 @@ function addGalleryItems(items) {
       return `
     <div class="gallery__item">
         <a class="gallery__link" href="${original}">
-            <img 
-                class="gallery__image" 
-                src="${preview}" 
-                data-source="${original}" 
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
                 alt="${description}"
             />
         </a>
@@ -25,17 +25,28 @@ function addGalleryItems(items) {
     `;
     })
     .join("");
-
-  console.log(markup);
 }
+
+container.addEventListener("click", openFullImg);
 
 function openFullImg(event) {
   event.preventDefault();
 
-  const isGalleryImageEl =
-    event.target.classList.contain("gallery__image");
-
-  if (!isGalleryImageEl) {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.sourse}" width="800" height="600">
+`);
+
+  instance.show();
+
+  container.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+
+  console.log(event);
 }
