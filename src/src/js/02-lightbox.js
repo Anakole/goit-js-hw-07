21aae2d1054f55e281601e4e3a16dc28cb662072
Anakole <104356239+Anakole@users.noreply.github.com>
2022-09-21 18:48:21 +0300

    edits

--- src/src/js/02-lightbox.js
+++ src/src/js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+const createGallery = addGalleryItems(galleryItems);
+
+container.insertAdjacentHTML("beforeend", createGallery);
+
+function addGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery-link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+container.addEventListener("click", openFullImg);
+
+const simple = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+function openFullImg(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  console.log(event);
+}
